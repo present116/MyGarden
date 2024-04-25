311758f168318a2b009847e558d2f87e81926fe3
고현재 <hjko@gohyeonjaeui-MacBookAir.local>
2024-04-25 13:58:14 +0900

    화면 validation체크

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -12,21 +12,55 @@ function RegisterPage() {
     const [Password, setPassword] = useState("");
     const [ConfirmPassword, setConfirmPassword] = useState("");
 
-   
+    const [EamilErr, setEmailErr] = useState("");
+    const [PasswordErr, setPasswordErr] = useState("");
+    const [ConfirmPwErr, setConfirmPwErr] = useState("");
+    const [NameErr, setNameErr] = useState("");
+
+
     const onEmailHandler = (event) => {
-        setEmail(event.currentTarget.value);
+        setEmail(event.currentTarget.value);  
+        
+        const pattern=/^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z]?)*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+        if(!pattern.test(event.currentTarget.value)){
+            setEmailErr(true);       
+        }else{
+            setEmailErr(false);       
+        }
+        
     }
 
     const onNameHandler = (event) => {
         setName(event.currentTarget.value);
+        const pattern = /^[a-zA-Zㄱ-힣][a-zA-Zㄱ-힣 ]*$/;
+        if(!pattern.test(event.currentTarget.value)){
+            setNameErr(true);       
+        }else{
+            setNameErr(false);       
+        }
     }
 
     const onPasswordHandler = (event) => {
         setPassword(event.currentTarget.value)
+
+        const pattern =  /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/
+        if(!pattern.test(event.currentTarget.value)){
+            setPasswordErr(true);       
+        }else{
+            setPasswordErr(false);       
+        }
     }
 
     const onConfirmPasswordHandler = (event) => {
         setConfirmPassword(event.currentTarget.value)
+
+        const pattern = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/
+        if(!pattern.test(event.currentTarget.value)){
+            setConfirmPwErr(true);       
+        }else{
+            setConfirmPwErr(false);       
+        }
+
     }
 
     const onSubmitHandler = (event) => {
@@ -67,17 +101,25 @@ function RegisterPage() {
                             <h3 class="tit_sub">독서의 정원에 오신걸 환영합니다. <br />회원가입 하시면 더 많은 서비스를 이용하실 수 있습니다.</h3>
                             <div className="form_section">
                                 <div className="form_list">
-                                    <div className="form_item user" id="divId">
-                                        <input type="text" id="id" name="id" placeholder="이메일" className="input" value={Email} onChange={onEmailHandler} maxLength="50" autoCapitalize="off" />
+                                    <div className="form_item email" id="divId">
+                                        <input type="text" id="id" name="id" placeholder="이메일" 
+                                        className = {[EamilErr ? 'err' : '', 'input'].join(" ")}
+                                        value={Email} onChange={onEmailHandler} maxLength="50" autoCapitalize="off" />
                                     </div>
                                     <div className="form_item lock password" id="divPasswd">
-                                        <input type="password" id="pswd1" name="pswd1" placeholder="비밀번호" className="input" value={Password} onChange={onPasswordHandler} maxLength="20" autocomplete="password" />
+                                        <input type="password" id="pswd1" name="pswd1" placeholder="비밀번호" 
+                                        className = {[PasswordErr ? 'err' : '', 'input'].join(" ")}
+                                        value={Password} onChange={onPasswordHandler} maxLength="20" autocomplete="password" />
                                     </div>
                                     <div className="form_item lock password" id="divPasswd">
-                                        <input type="password" id="pswd2" name="pswd2" placeholder="비밀번호 확인" className="input" value={ConfirmPassword} onChange={onConfirmPasswordHandler} maxLength="20" autocomplete="new-password" />
+                                        <input type="password" id="pswd2" name="pswd2" placeholder="비밀번호 확인" 
+                                        className = {[ConfirmPwErr ? 'err' : '', 'input'].join(" ")}
+                                        value={ConfirmPassword} onChange={onConfirmPasswordHandler} maxLength="20" autocomplete="new-password" />
                                     </div>
                                     <div className="form_item user" id="divName">
-                                        <input type="text" id="name" name="name" placeholder="이름" className="input" value={Name} onChange={onNameHandler} maxLength="40" />
+                                        <input type="text" id="name" name="name" placeholder="이름" 
+                                        className= {[NameErr ? 'err' : '', 'input'].join(" ")}
+                                        value={Name} onChange={onNameHandler} maxLength="40" />
                                     </div>
                                 </div>
                             </div>
